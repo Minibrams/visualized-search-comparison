ccc106130308b057e1070fa9e1f95a8fe9beef7c
Minibrams <anders@brams.dk>
2018-01-30 17:54:42 +0100

    Making sure the p5.js works as expected

---
+++ main.js
@@ -0,0 +1,17 @@
+var p5js = function (p) {
+    
+    p.setup = function() {
+        // Save height and width for later positioning
+        let cnv = document.getElementById('theCanvas');
+        p.width = cnv.offsetWidth;
+        p.height = cnv.offsetHeight;
+        p.createCanvas(p.width, p.height);
+        p.background(0);
+    }
+
+    p.draw = function () {
+        p.background(0);
+    }
+} 
+
+var theP5Sketch = new p5(p5js, 'theCanvas');
