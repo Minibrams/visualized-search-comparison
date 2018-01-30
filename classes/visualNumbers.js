e1fc4bb27a45d1989da1251dcd43823df5dcdaf2
Minibrams <anders@brams.dk>
2018-01-30 18:23:11 +0100

    Visualization works with single digit numbers

--- classes/visualNumbers.js
+++ classes/visualNumbers.js
@@ -1,11 +1,12 @@
-function visualNumber (val, x, y) {
-    
+function visualNumber (sketch, val, x, y) {
+    this.p = sketch;
     this.val = val;
     this.x = x;
     this.y = y;
 
     this.show = function () {
-        ellipse(this.x, this.y, 10, 10);
-        text(this.val, this.x, this.y);    
+        let textOffset = FONT_SIZE * 0.33;
+        this.p.ellipse(this.x, this.y, 20, 20);
+        this.p.text(this.val, this.x - textOffset, this.y + textOffset);    
     }
 }
